41c53c581439c3480a9de1168a61b21cca61ac70
Mohamed Amoussa <mohamedj81@hotmail.com>
2024-05-28 16:15:39 +0200

    integrate home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <section className="text-center h-screen">
+        <h1 className="font-bold text-3xl mt-20">Home</h1>
+        <Link
+          className="text-gray-900 leading-7 hover:text-gray-900/70 underline"
+          href="/create-post">
+          Create New Book
+        </Link>
+      </section>{" "}
     </div>
   );
 }
